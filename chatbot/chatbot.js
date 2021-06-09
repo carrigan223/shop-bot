8917b26602d12a2cef400a724ebe707ffaaa650b
Andrew Carrigan <carrigan223@gmail.com>
2021-06-08 21:44:33 -0700

    got it working at the moment

---
+++ chatbot/chatbot.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const dialogflow = require("dialogflow");
+const config = require("../config/keys");
+
+const sessionClient = new dialogflow.SessionsClient();
+
+const sessionPath = sessionClient.sessionPath(
+  config.googleProjectID,
+  config.dialogFlowSessionID
+);
+
+//handling the dialogflow implementation
+module.exports = {
+  textQuery: async function (text, parameters = {}) {
+    //bringing in the handleAction() method to be called before initialization
+    //'self' is the module we are working in
+    let self = module.exports; 
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: text,
+          languageCode: config.dialogFlowSessionLanguageCode,
+        },
+      },
+      queryParams: {
+        payload: {
+          data: parameters,
+        },
+      },
+    };
+    let responses = await sessionClient.detectIntent(request);
+    responses = await self.handleAction(responses);
+    return responses;
+  },
+
+  handleAction: function (responses) {
+    return responses;
+  },
+};
