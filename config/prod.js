10d3f41fc432a10b50e029e8dd01135f6833122d
Andrew Carrigan <carrigan223@gmail.com>
2021-06-09 21:43:59 -0700

    lets try this

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,8 @@
-const privateKey = process.env.GOOGLE_PRIVATE_KEY;
 
 module.exports = {
   googleProjectID: process.env.GOOGLE_PROJECT_ID,
   dialogFlowSessionID: process.env.DIALOG_FLOW_SESSION_ID,
   dialogFlowSessionLanguageCode: process.env.DIALOG_FLOW_LANGUAGE_CODE,
   googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-  googlePrivateKey: JSON.parse(privateKey),
+  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
 };
