391871d9108c8e0980365da544b817ad54425505
Andrew Carrigan <carrigan223@gmail.com>
2021-06-09 21:42:27 -0700

    trying

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
-const privateKey = JSON.parse(process.env.GOOGLE_PRIVATE_KEY);
+const privateKey = process.env.GOOGLE_PRIVATE_KEY;
 
 module.exports = {
   googleProjectID: process.env.GOOGLE_PROJECT_ID,
   dialogFlowSessionID: process.env.DIALOG_FLOW_SESSION_ID,
   dialogFlowSessionLanguageCode: process.env.DIALOG_FLOW_LANGUAGE_CODE,
   googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-  googlePrivateKey: privateKey,
+  googlePrivateKey: JSON.parse(privateKey),
 };
