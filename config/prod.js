9637ce63c223dbc9142ef337f59b60b1c5d9d258
Andrew Carrigan <carrigan223@gmail.com>
2021-06-09 20:19:05 -0700

    try 3

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
   googleProjectID: process.env.GOOGLE_PROJECT_ID,
   dialogFlowSessionID: process.env.DIALOG_FLOW_SESSION_ID,
   dialogFlowSessionLanguageCode: process.env.DIALOG_FLOW_LANGUAGE_CODE,
-  googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+  googleClientEmail: JSON.parse(process.env.GOOGLE_CLIENT_EMAIL),
   googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
 };
