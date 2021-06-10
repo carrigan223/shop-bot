ecf1d1b64e1b9945ad28ef3930dd56eb5c4c8697
Andrew Carrigan <carrigan223@gmail.com>
2021-06-10 01:08:48 -0700

    make better

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ require("./routes/dialogflowRoutes")(app);
 
 //setting the port to be used
 //specified by .env otherwise default to
-//port 5000 for local
+//port 5000 for development
 const PORT = process.env.PORT || 5000;
 
 //set the port for the app to listen
 app.listen(PORT, () => {
-  console.log(config);
+  console.log(config.googlePrivateKey);
   console.log(`Server is now listening on port ${PORT}`);
 });
