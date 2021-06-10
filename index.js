19f0a88c25f4562ab064e3fd7a5f94199f234236
Andrew Carrigan <carrigan223@gmail.com>
2021-06-09 22:55:32 -0700

    heroku

--- index.js
+++ index.js
@@ -18,4 +18,5 @@ const PORT = process.env.PORT || 5000;
 //set the port for the app to listen
 app.listen(PORT, () => {
   console.log(`Server is now listening on port ${PORT}`);
+  console.log(config.keys.googlePrivateKey)
 });
