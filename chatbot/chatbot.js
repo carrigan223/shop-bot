a118b16ca2624761632eae2fa58e99c66b59766c
Andrew Carrigan <carrigan223@gmail.com>
2021-06-10 23:14:49 -0700

    The final countdown

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -5,7 +5,6 @@ const { struct } = require("pb-util");
 const nonGoogleConfig = require("../config/keys");
 const config = require("../google-credentials.json");
 
-
 const projectId = config.project_id;
 
 const credentials = {
@@ -14,21 +13,18 @@ const credentials = {
 };
 
 const sessionClient = new dialogflow.SessionsClient({ projectId, credentials });
-
+const sessionPath = sessionClient.sessionPath(
+  nonGoogleConfig.googleProjectID,
+  nonGoogleConfig.dialogFlowSessionID
+);
 //handling the dialogflow implementation
 module.exports = {
-
   textQuery: async function (text, parameters = {}) {
     //bringing in the handleAction() method to be called before initialization
     //'self' is the module we are working in
     let self = module.exports;
-    const sessionPath = sessionClient.sessionPath(
-      nonGoogleConfig.googleProjectID,
-      nonGoogleConfig.dialogFlowSessionID
-    );
 
     const request = {
-
       session: sessionPath,
       queryInput: {
         text: {
@@ -51,6 +47,7 @@ module.exports = {
     //bringing in the handleAction() method to be called before initialization
     //'self' is the module we are working in
     let self = module.exports;
+
     const request = {
       session: sessionPath,
       queryInput: {
